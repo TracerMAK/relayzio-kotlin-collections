116e041daa2cecec84fe5f443123b77169f2f197
Mark <tsmckiel@live.com>
2020-01-11 12:41:24 -0600

    Update: OrderedSets.kt

--- src/main/kotlin/com/relayzio/kotlin/Main.kt
+++ src/main/kotlin/com/relayzio/kotlin/Main.kt
@@ -1,3 +1,18 @@
 package com.relayzio.kotlin
 
-fun main(args: Array<String>) = println("Running...")
+import kotlin.math.*
+import com.relayzio.kotlin.collections.orderedSetByWeightOf
+
+fun main(args: Array<String>) {
+    // Odd numbers have higher weight.
+    val oddToEven: (Int) -> Int = {x -> x % 2}
+    val arr = arrayOf(1,5,2,3,10,7,15)
+    val s = orderedSetByWeightOf(*arr, weight = oddToEven)
+    s.forEach() {print("${it} ")}
+    println()
+    
+    val meanSquaredError: (Double) -> Double = {x ->  0 - (x - 15.25).pow(2)}
+    val arrEmpty = arrayOf<Double>(14.2, 14.8, 16.1, 15.32, 15.25)
+    val s2 = orderedSetByWeightOf(*arrEmpty, weight = meanSquaredError)
+    s2.forEach() {print("${it} ")}
+}
--- src/main/kotlin/com/relayzio/kotlin/collections/OrderedSets.kt
+++ src/main/kotlin/com/relayzio/kotlin/collections/OrderedSets.kt
@@ -1,11 +1,25 @@
 package com.relayzio.kotlin.collections
 
 /**
- * Creates a sorted set from the provided array and weight generation function.
+ * Creates a read-only ordered set from the provided arguments and weight
+ * generation function.
  */
-public fun <E> orderedSetByWeightOf(vararg elements: E, weight: (E) -> Int) : Set<E> =
-    if (elements.size > 0) elements.sortedBy(weight).toSet() else emptySet()
+@JvmName("orderedSetByWeightIntOf")
+inline fun <reified E> orderedSetByWeightOf(vararg elements: E, crossinline weight: (E) -> Int) : Set<E> =
+    when (elements.size) {
+        0 -> emptySet()
+        else -> elements.sortedByDescending(weight).toSet()
+    }
+
+@JvmName("orderedSetByWeightDoubleOf") 
+inline fun <reified E> orderedSetByWeightOf(vararg elements: E, crossinline weight: (E) -> Double) : Set<E> =
+    when (elements.size) {
+        0 -> emptySet()
+        else -> elements.sortedByDescending(weight).toSet()
+    }
+
 
 public interface OrderedSet<out E> : Set<E> {
     // Additional operations for an ordered set.
+   
 }
