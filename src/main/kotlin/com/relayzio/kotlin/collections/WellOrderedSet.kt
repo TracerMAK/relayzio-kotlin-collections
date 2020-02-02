4f16729af2d41e3eac7750cad28b76ce73cb387b
Mark <tsmckiel@live.com>
2020-02-01 23:59:02 -0600

    WellOrderedSet.kt : Update#2

--- src/main/kotlin/com/relayzio/kotlin/collections/WellOrderedSet.kt
+++ src/main/kotlin/com/relayzio/kotlin/collections/WellOrderedSet.kt
@@ -14,22 +14,29 @@ sealed class WellOrderedSet<out E> : OrderedSet<E>, Serializable {
 	override abstract fun isEmpty(): Boolean
 	override abstract fun iterator(): Iterator<@UnsafeVariance E>
 
-    internal object Empty : WellOrderedSet<Nothing>() {
+    internal class Empty : WellOrderedSet<Nothing>() {
 	    override val size: Int = 0
 		override fun contains(element: Nothing): Boolean = false
-		override fun containsAll(elements: Collection<Nothing>): Boolean = false
-		
+		override fun containsAll(elements: Collection<Nothing>): Boolean = false		
 		override fun isEmpty() = true
 		
-		override fun iterator(): Iterator<Nothing> = TODO("")
+		override fun iterator(): Iterator<Nothing> = object : AbstractIterator<Nothing>() {
+		    override fun computeNext() = done()
+		}
 	}
 	
-	
-	/*
 	internal class Cons<E>(internal val head: E,
-	                       internal val tail: WellOrderedSet<E>) : WellOrderedSet<E> {
+	                       internal val tail: WellOrderedSet<E>) : WellOrderedSet<E>() {
 						   
-	}*/
-	
+		override val size: Int = tail.size + 1
+		
+        override fun contains(element: @UnsafeVariance E): Boolean = TODO("")
+        override fun containsAll(elements: Collection<@UnsafeVariance E>): Boolean = TODO("")
+		
+        override fun isEmpty() = false
 
+        override fun iterator(): Iterator<@UnsafeVariance E> = TODO("")
+				   
+	}
+	
 }
